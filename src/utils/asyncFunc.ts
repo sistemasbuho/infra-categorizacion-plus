6bb0986afc3125e66f0145aa96e138d56a779195
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-14 09:25:06 -0500

    create: AsyncSelect functions

--- src/utils/asyncFunc.ts
+++ src/utils/asyncFunc.ts
@@ -1,9 +1,47 @@
-import { generalRequest } from './funcs';
+import { GeneralRequest } from './funcs';
+
+/*
+============================================================
+==    Todas las funciones asincronas deberán estar aquí   ==
+============================================================
+*/
 
 export async function getArticleData(id: string | number) {
-  return await generalRequest(`articulos/${id}/fragmentos`);
+  console.log(GeneralRequest(`articulos/${id}/fragmentos`));
+  return await GeneralRequest(`articulos/${id}/fragmentos`);
 }
 
-export async function getTagCategorization(body) {
-  return await generalRequest(`buscar_variables`, 'POST', { body });
+export async function getTagCategorization(query: string, projectId: number) {
+  return await GeneralRequest(`buscar_variables`, 'POST', {
+    id: Date.now(),
+    nombre: query,
+    modelo: 'Tag',
+    proyecto_id: projectId,
+  });
+}
+
+export async function getTemaCategorization(query, projectId) {
+  return await GeneralRequest('buscar_variables', 'POST', {
+    id: Date.now(),
+    nombre: query,
+    modelo: 'Tema',
+    proyecto_id: projectId,
+  });
+}
+
+export async function getTonoCategorization(query: string) {
+  return await GeneralRequest('buscar_variables', 'POST', {
+    id: Date.now(),
+    nombre: query,
+    modelo: 'Tono',
+  });
+}
+
+// esta petición es para los activos y los pasivos
+export async function getActivoPasivoCategorization(query: string) {
+  return await GeneralRequest('buscar_variables', 'POST', {
+    id: Date.now(),
+    nombre: query,
+    modelo: 'Actor',
+  });
 }
