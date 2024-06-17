298336a6f3a04cbe16c67789bd64f613f26cd78a
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-17 09:37:05 -0500

    create interfaces

--- src/interfaces/generals.ts
+++ src/interfaces/generals.ts
@@ -4,12 +4,32 @@ export interface article {
   text: string;
 }
 
-export interface categorization {
+export interface headerArticle extends article {
+  medio?: { id: number; nombre: string };
+  autor?: { id: number; nombre: string };
+  tipo_articulo?: { id: number; nombre: string };
+  programa?:{ id: number; nombre: string };
+  fecha?: string;
+}
+
+// export interface categorization {
+//   tono: number;
+//   tag?: number;
+//   tema: number;
+//   activo?: number;
+//   pasivo?: number;
+// }
+
+export interface newCategorization {
+  article_fragment: string;
+  start_index: number;
+  end_index: number;
+  articulo: number;
+  tag: number[];
+  tema: number[];
   tono: number;
-  tag?: number;
-  tema: number;
-  activo?: number;
-  pasivo?: number;
+  activo: string[];
+  pasivo: string[];
 }
 
 export interface GeneralRequestOptions {
