cc88f25e98fcd39e97992978d07060bd8fdaf0fa
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-19 18:05:25 -0500

    create exit function

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -2,11 +2,17 @@ import { faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from '../assets/css/components/headerBar.module.css';
+import { useNavigate } from 'react-router';
 
 function HeaderBar() {
+  const navigate = useNavigate();
+  function exit() {
+    navigate(-1);
+  }
+
   return (
     <nav className={styles.container}>
-      <div>
+      <div onClick={exit}>
         <FontAwesomeIcon icon={faCaretLeft} />
         <p>Atrás</p>
       </div>
