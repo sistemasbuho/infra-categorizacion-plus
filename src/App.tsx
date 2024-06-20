3f6b58eb29a6df4f49a11aa45cbbcf7b11eca3a1
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-20 11:52:31 -0500

    create * route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
         <Route path="/articulo/:id" element={<GlobalComponent />} />
       </Routes>
     </BrowserRouter>
