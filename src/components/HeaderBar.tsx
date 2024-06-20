6e3b2d4a959c3519caa195e1de9cad14dbee3831
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-20 18:12:58 -0500

    create validation to render delete article modal

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -3,27 +3,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from '../assets/css/components/headerBar.module.css';
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
+import ConfirmDeleteArticle from './menu/options/ConfirmDeleteArticle';
+import { article } from '../interfaces/generals';
 
-function HeaderBar() {
+interface Props {
+  article: article;
+}
+
+function HeaderBar({ article }: Props) {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
   const navigate = useNavigate();
   function exit() {
     navigate(-1);
   }
 
   return (
-    <nav className={styles.container}>
-      <div onClick={exit}>
-        <FontAwesomeIcon icon={faCaretLeft} />
-        <p>Atrás</p>
-      </div>
+    <>
+      <nav className={styles.container}>
+        <div onClick={exit}>
+          <FontAwesomeIcon icon={faCaretLeft} />
+          <p>Atrás</p>
+        </div>
 
-      <div className="d-flex gap-3">
-        <button className={`${styles.btn} ${styles.btn_delete}`}>
-          Eliminar articulo
-        </button>
-        <button className={`${styles.btn} ${styles.btn_finish}`}>Finalizar</button>
-      </div>
-    </nav>
+        <div className="d-flex gap-3">
+          <button
+            className={`${styles.btn} ${styles.btn_delete}`}
+            onClick={() => setShowDeleteModal(true)}
+          >
+            Eliminar articulo
+          </button>
+          <button className={`${styles.btn} ${styles.btn_finish}`}>
+            Finalizar
+          </button>
+        </div>
+      </nav>
+
+      {showDeleteModal && (
+        <ConfirmDeleteArticle article={article} setShow={setShowDeleteModal} />
+      )}
+    </>
   );
 }
 
