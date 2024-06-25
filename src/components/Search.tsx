983e6ca4e543d65d81f2bd2255a4150ef6b1f144
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-25 00:45:58 -0500

    create input control to search input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,11 +2,23 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from '../assets/css/components/search.module.css';
+import { Dispatch, SetStateAction } from 'react';
 
-function Search() {
+interface Props {
+  state: string | null;
+  setState: Dispatch<SetStateAction<string>>;
+}
+
+function Search({ state, setState }: Props) {
   return (
     <div className={styles.container_search}>
-      <input type="text" id="search" placeholder='Buscar'/>
+      <input
+        type="text"
+        id="search"
+        placeholder="Buscar"
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+      />
 
       <button>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
