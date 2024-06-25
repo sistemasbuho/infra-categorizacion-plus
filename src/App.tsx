824c152c8474bb2015a5b36514a5b0c9aefd35c3
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-06-25 13:19:09 -0500

    modify browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
+import { ConfigProvider } from './context/ConfigContext';
+import { ArticleProvider } from './context/ArticleContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalComponent from './GlobalComponent';
 import Home from './pages/Home';
-import { ConfigProvider } from './context/ConfigContext';
-import { ArticleProvider } from './context/ArticleContext';
 
 function App() {
   return (
+        <BrowserRouter>
     <ConfigProvider>
       <ArticleProvider>
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/*" element={<Home />} />
             <Route path="/articulo/:id" element={<GlobalComponent />} />
           </Routes>
-        </BrowserRouter>
       </ArticleProvider>
     </ConfigProvider>
+        </BrowserRouter>
   );
 }
 
