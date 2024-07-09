ac68c10bc009238ad54ca5a194b1dedb5d0d0c5d
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-07-09 11:17:46 -0500

    styles from toast error

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,21 @@ function App() {
         toastOptions={{
           success: {
             icon: <FontAwesomeIcon icon={faCircleCheck} fontSize={25} />,
+            style: {
+              padding: '1rem',
+              border: '2px solid var(--border-toast-succes)',
+              color: 'var(--color-toast-succes)',
+              backgroundColor: 'var(--bg-toast-succes)',
+            },
           },
           error: {
             icon: <FontAwesomeIcon icon={faCircleCheck} fontSize={25} />,
-          },
-          style: {
-            padding: '1rem',
-            border: '2px solid var(--border-toast-succes)',
-            color: 'var(--color-toast-succes)',
-            backgroundColor: 'var(--bg-toast-succes)',
+            style: {
+              padding: '1rem',
+              border: '2px solid var(--border-toast-error)',
+              color: 'var(--color-toast-error)',
+              backgroundColor: 'var(--bg-toast-error)',
+            },
           },
         }}
       />
