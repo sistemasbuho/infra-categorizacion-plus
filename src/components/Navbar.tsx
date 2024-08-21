4fc0e892bfa8d7d167f7ea44ddbb50bbd59fa2bf
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-08-21 17:54:44 -0500

    add icon in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import { useArticleContext } from '../context/ArticleContext';
 import { useState } from 'react';
 
 import styles from '../assets/css/components/navbar.module.css';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Navbar() {
   const [open, setOpen] = useState(false);
@@ -16,13 +18,23 @@ function Navbar() {
 
   return (
     <div className={styles.container}>
-      <h1
-        onClick={() => setOpen((prev) => !prev)}
-        className={` ${open ? styles.open : ''} `}
-        onDoubleClick={goToOriginalLink}
+      <div
+        className="d-flex align-items-center gap-3 "
+        style={{ cursor: 'pointer' }}
       >
-        {titulo}
-      </h1>
+        <FontAwesomeIcon
+          icon={faArrowUpRightFromSquare}
+          onClick={goToOriginalLink}
+        />
+
+        <h1
+          onClick={() => setOpen((prev) => !prev)}
+          className={` ${open ? styles.open : ''} `}
+          onDoubleClick={goToOriginalLink}
+        >
+          {titulo}
+        </h1>
+      </div>
       <p>{proyecto}</p>
     </div>
   );
